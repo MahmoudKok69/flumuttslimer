d3a508c4f245444c7dff1d01ba174bd2fb46ccb2
mahmoudkok69 <mahmoudkokeh462@gmail.com>
2023-10-19 16:45:22 +0300

    setup the project

---
+++ lib/core/colors.dart
@@ -0,0 +1,4 @@
+//main colors
+
+
+//other colors
---
+++ lib/core/strings.dart
@@ -0,0 +1,9 @@
+String wellcomeMessage = '';
+
+String NO_INTERNET_CONECATION_ERROR = '';
+
+String LOG_IN_FALILD = '';
+
+String LOG_IN_SUCCESS = '';
+
+String NO_USER_ERROR = '';
